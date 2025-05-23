dbd5af766ca6f7552b5eba842e74821911fc53de
FJAO87 <javierarrabalgithub@gmail.com>
2025-05-23 12:46:14 +0200

    Update GymvaDbContext.cs

--- GymvaDbContext.cs
+++ GymvaDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using GymvaActivacionWeb.Models;
 
-namespace GymvaActivacionWeb.Data
+namespace GymvaActivacionWeb.Data/GymvaDbContext.cs
 {
     public class GymvaDbContext : DbContext
     {
